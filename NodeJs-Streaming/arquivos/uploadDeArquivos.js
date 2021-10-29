e6ba640b60f0119646b89a362ad9bf90860dff45
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-10-29 16:12:30 -0500

    tratando erros

--- NodeJs-Streaming/arquivos/uploadDeArquivos.js
+++ NodeJs-Streaming/arquivos/uploadDeArquivos.js
@@ -5,17 +5,22 @@ module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) =>{
 
     const tiposValidos = ['jpg', 'png', 'jpeg']
     const tipo = path.extname(caminho)
-    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
 
-    if (tipoEhValido === -1){
-        console.log('Tipo invalido')
-    } else {
+    if (tipoEhValido){
         const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
 
         fs.createReadStream(caminho)
         .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+        .on('finish', () => callbackImagemCriada(false, novoCaminho))
+
+    } else {
+        const erro = "Tipo invalido"
+        console.log('Tipo invalido')
+        callbackImagemCriada(erro)
     }
 
 }
+
+
 
