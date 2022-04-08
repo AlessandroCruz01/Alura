ea9d34777c7db2d098a095145d4f67973f0043f0
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2022-04-07 23:30:41 -0500

    validacoes e refatoracoes do codigo

--- Node/SequelizeMysql-ORMNode/api/controllers/TurmaController.js
+++ Node/SequelizeMysql-ORMNode/api/controllers/TurmaController.js
@@ -12,6 +12,63 @@ class TurmaController {
         return res.status(500).json(error.message);
       }
     }
+
+    static async pegaUmaTurma(req, res) {
+      const { id } = req.params
+      try {
+        const umaTurma = await db.Turmas.findOne( { 
+          where: { 
+            id: Number(id) 
+          }
+        })
+        return res.status(200).json(umaTurma)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async criaTurma(req, res) {
+      const novaTurma = req.body
+      try {
+        const novaTurmaCriada = await db.Turmas.create(novaTurma)
+        return res.status(200).json(novaTurmaCriada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async atualizaTurma(req, res) {
+      const { id } = req.params
+      const novasInfos = req.body
+      try {
+        await db.Turmas.update(novasInfos, { where: { id: Number(id) }})
+        const turmaAtualizada = await db.Turmas.findOne( { where: { id: Number(id) }})
+        return res.status(200).json(turmaAtualizada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async apagaTurma(req, res) {
+      const { id } = req.params
+      try {
+        await db.Turmas.destroy({ where: { id: Number(id) }})
+        return res.status(200).json({ mensagem: `id ${id} deletado` })
+  
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async restauraTurma(req, res) {
+      const { id } = req.params
+      try {
+        await db.Turmas.restore( {where: { id: Number(id) } } )
+        return res.status(200).json({ mensagem: `id ${id} restaurado`})
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
 }
 
 module.exports = TurmaController
